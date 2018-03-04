4fcf25512694d3c8693ff9aa0bcb06791857ad92
CloudCannon <support@cloudcannon.com>
2018-03-04 20:48:34 +0000

    Updated 30 files via CloudCannon

---
+++ github-web-hook.php
@@ -0,0 +1,40 @@
+<?php
+
+if (validate()) {
+  $gitDir = __DIR__;
+  exec("git -C {$gitDir} pull -f 2>&1", $gitOutput);
+  file_put_contents('../log/last_deploy.log', $gitOutput);
+}
+
+function validate()
+{
+    $signature = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
+    $event = @$_SERVER['HTTP_X_GITHUB_EVENT'];
+    $delivery = @$_SERVER['HTTP_X_GITHUB_DELIVERY'];
+
+    if (!isset($signature, $event, $delivery)) {
+        return false;
+    }
+
+    $payload = file_get_contents('php://input');
+
+    if (!validateSignature($signature, $payload)) {
+        return false;
+    }
+
+    //$data = json_decode($payload,true);
+    return true;
+}
+
+function validateSignature($gitHubSignatureHeader, $payload)
+{
+    list ($algo, $gitHubSignature) = explode("=", $gitHubSignatureHeader);
+
+    if ($algo !== 'sha1') {
+        // see https://developer.github.com/webhooks/securing/
+        return false;
+    }
+
+    $payloadHash = hash_hmac($algo, $payload, getenv('GITHUB_WEB_HOOK_SECRET'));
+    return ($payloadHash === $gitHubSignature);
+}
