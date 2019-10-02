8723c797bb65ee0166717a8caf8b358c8eac03ba
CloudCannon <support@cloudcannon.com>
2019-10-02 08:50:15 +0000

    Updated 40 files via CloudCannon

--- js/javascript.js
+++ js/javascript.js
@@ -237,3 +237,11 @@ function is_youtubelink(url) {
       
 
   });
+
+function toggleDarkmode() {
+    document.getElementById('top').classList.toggle('darkmode'); 
+    document.getElementById('darkmode').classList.toggle('selected'); 
+    if (localStorage.getItem('mode') != 'dark') localStorage.setItem('mode', 'dark'); 
+    else localStorage.setItem('mode', 'normal');
+    document.getElementById('darkmode').blur();
+}
