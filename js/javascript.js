461ea59db18c19d9628102ab3cd610b55458dfc1
CloudCannon <support@cloudcannon.com>
2020-02-23 23:34:06 +0000

    Updated 'js/javascript.js' via CloudCannon

--- js/javascript.js
+++ js/javascript.js
@@ -184,10 +184,17 @@ function is_youtubelink(url) {
 
   });
 
-function toggleDarkmode() {
-    document.getElementById('top').classList.toggle('darkmode'); 
-    document.getElementById('darkmode').classList.toggle('selected'); 
-    if (localStorage.getItem('mode') != 'dark') localStorage.setItem('mode', 'dark'); 
-    else localStorage.setItem('mode', 'normal');
-    document.getElementById('darkmode').blur();
+function toggleLightmode() {
+    if(document.getElementById('top').classList.contains('lightmode')){
+        document.getElementById('top').classList.remove('lightmode');
+        document.getElementById('top').classList.add('darkmode');
+        localStorage.setItem('mode', 'dark'); 
+    } else if (document.getElementById('top').classList.contains('darkmode')) {
+        document.getElementById('top').classList.remove('darkmode');
+        localStorage.setItem('mode', 'blue'); 
+    } else {
+        document.getElementById('top').classList.add('lightmode');
+        localStorage.setItem('mode', 'light'); 
+    }
+    document.getElementById('lightmode').blur();
 }
